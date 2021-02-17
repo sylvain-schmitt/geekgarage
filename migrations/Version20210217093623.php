2bbe9ecc0141d850b688578d18a980beb3f2a9d7
sylvain <devil.9068@gmail.com>
2021-02-17 10:39:36 +0100

    add config heroku

--- migrations/Version20210217093623.php
+++ migrations/Version20210217093623.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210120082113 extends AbstractMigration
+final class Version20210217093623 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210120082113 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE agency (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, city VARCHAR(255) NOT NULL, lat NUMERIC(10, 8) NOT NULL, lon NUMERIC(10, 8) NOT NULL, number VARCHAR(100) NOT NULL, mail VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, comment CLOB NOT NULL)');
+        $this->addSql('CREATE TABLE agency (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, city VARCHAR(255) NOT NULL, lat NUMERIC(10, 8) NOT NULL, lon NUMERIC(10, 8) NOT NULL, number VARCHAR(100) NOT NULL, mail VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, comment CLOB NOT NULL, count INTEGER NOT NULL)');
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
         , password VARCHAR(255) NOT NULL)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
